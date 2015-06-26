76b12a80fc66b9c5213d6e291e95c92a49efb25d
Normand Poirier <npoirier@gmail.com>
2015-06-26 11:48:40 -0400

    Init Spring boot project

---
+++ src/main/java/com/proto/ProtodirectApplication.java
@@ -0,0 +1,12 @@
+package com.proto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtodirectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtodirectApplication.class, args);
+    }
+}
